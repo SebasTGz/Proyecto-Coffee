c9f482a9da984d1c31832cd460fa2517e57e08e5
SebasTGz <123501858+SebasTGz@users.noreply.github.com>
2024-09-24 13:01:21 -0500

    Reorganización Carpetas

--- Coffee-main/funcionalidad/script.js
+++ Coffee-main/funcionalidad/script.js
@@ -2,7 +2,7 @@ const carrito = document.getElementById('carrito');
 const elementos1 = document.getElementById('lista-1');
 const lista = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoBtn = document.getElementById('vaciar-carrito');
-//const totalCarritoBtn = document.getElementById('total-carrito');
+const totalCarritoBtn = document.getElementById('total-carrito');
 
 cargarEventListeners();
 
@@ -11,7 +11,7 @@ function cargarEventListeners() {
     elementos1.addEventListener('click', comprarElemento);
     carrito.addEventListener('click', eliminarElemento);
     vaciarCarritoBtn.addEventListener('click', vaciarCarrito);
-    //totalCarritoBtn.addEventListener(totalCarritoBtn);
+    totalCarritoBtn.addEventListener(totalCarritoBtn);
 
 }
 
